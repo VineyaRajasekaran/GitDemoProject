263218b07b37d8164c4f48647fe918167908e066
VineyaRajasekaran <vineya@gmail.com>
2018-04-05 14:15:17 -0500

    First CodeCheckin

---
+++ GitStuff/src/test/java/SeleniumLearning/AppiumTest.java
@@ -0,0 +1,24 @@
+package SeleniumLearning;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void helloWorldAppium()
+	{
+		System.out.println("helloAppium");
+	}
+	
+	@Test
+	public void helloWorldAppium1()
+	{
+		System.out.println("helloAppium1");
+	}
+	@Test
+	public void helloWorldAppium3()
+	{
+		System.out.println("helloAppium3");
+	}
+
+}
---
+++ GitStuff/src/test/java/SeleniumLearning/TestNG2Test.java
@@ -0,0 +1,24 @@
+package SeleniumLearning;
+
+import org.testng.annotations.Test;
+
+public class TestNG2Test {
+	
+	@Test
+	public void helloWorldTestNG2()
+	{
+		System.out.println("helloTestNG2");
+	}
+	
+	@Test
+	public void helloWorldTestNG21()
+	{
+		System.out.println("helloTestNG21");
+	}
+	@Test
+	public void helloWorldTestNG23()
+	{
+		System.out.println("helloTestNG23");
+	}
+
+}
