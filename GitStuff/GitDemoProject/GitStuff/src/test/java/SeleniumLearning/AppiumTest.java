62d08777287aa7776aa1b7a0f495fc6680d36933
VineyaRajasekaran <vineya@gmail.com>
2018-04-05 14:37:07 -0500

    second code checkin

---
+++ GitStuff/GitDemoProject/GitStuff/src/test/java/SeleniumLearning/AppiumTest.java
@@ -0,0 +1,37 @@
+package SeleniumLearning;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void helloWorldAppium()
+	{
+		System.out.println("helloAppium");
+	}
+	
+	@Test
+	public void helloWorldAppium1()
+	{
+		System.out.println("helloAppium1");
+	}
+	@Test
+	public void helloWorldAppium3()
+	{
+		System.out.println("helloAppium3");
+	}
+	
+	@Test
+	public void testGit()
+	{
+		System.out.println("helloGit");
+	}
+	
+	@Test
+	public void testGitHub()
+	{
+		System.out.println("helloGitHub");
+	}
+
+
+}
